1d118f911eb247c2d567ba2068b56dd2552110e2
TylerMizuyabu <mizuyabu@hotmail.ca>
2016-10-20 12:18:05 -0400

    Updated class comment

--- Session.java
+++ Session.java
@@ -1,6 +1,6 @@
 /**
  * Abstract Class used to wrap Atm and Agent classes. Done in order to allow to 
- * store an atm instance or agent instance in a singular object without having to cast
+ * store an atm instance or agent instance in a singular object type Session without having to cast
  * the object when a method call is required. Defines all transactions that can be made
  * in all sessions. If another type of session is to be defined with a different set of
  * transactions. The transaction header would be defined here and session would extend this
