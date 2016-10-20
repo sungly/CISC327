7ef7522695c5c9adb855604d507d40608dfac20f
CalvinChe <calvinkpche@gmail.com>
2016-10-20 06:33:05 -0400

    Added comments to ATM and Agent, fixed a bug

--- Agent.java
+++ Agent.java
@@ -1,25 +1,25 @@
-public class Agent extends Atm{
-	public Agent(){
-		super(99999999);
+public class Agent extends Atm{ //subclass of the Atm class.
+	public Agent(){ //constructor class of an agent.
+		super(99999999); // Maximum amount that an account may withdraw/transfer/deposit from an account.
 	}//end constructor
 
-	public String create(String[] args){
-		if(args[1].length() >= 3 && args[1].length()<=30){
-			System.out.println("Creating account " + args[0]);
-			return "CR " + args[0] + " 00000000 000 " +args[1];
+	public String create(String[] args){ //Public method to create a new account.
+		if(args[1].length() >= 3 && args[1].length()<=30){ //if statement to check if the account name is within 3 and 30 alphanumeric characters.
+			System.out.println("Creating account " + args[0]); //Print out saying creating account + the account number.
+			return "CR " + args[0] + " 00000000 000 " +args[1]; //Returns a transaction message to put into the Transaction Summary File.
 		}
-		System.out.println("Error: account name must be between 3 and 30 alphanumberic characters");
-		return null;
+		System.out.println("Error: account name must be between 3 and 30 alphanumberic characters"); //Print out saying why the account was not created.
+		return null; //If account number is not valid,  then account is not created.  Returns null.
 
 	}//end end create
 
-	public String delete(String[] args){
-		if(args[1].length() >= 3 && args[1].length()<=30){
-			System.out.println("Deleting account " + args[0]);
-			return "DL " + args[0] + " 00000000 000 " +args[1];
+	public String delete(String[] args){ //Public method to delete an account.
+		if(args[1].length() >= 3 && args[1].length()<=30){ //if statement to check if the account name is within 3 and 30 alphanumeric characters.
+			System.out.println("Deleting account " + args[0]); //Print out saying Deleting account + The account number.
+			return "DL " + args[0] + " 00000000 000 " +args[1]; //Returns a transaction message to put into the Transaction Summary File.
 		}
-		System.out.println("Error: account name must be between 3 and 30 alphanumberic characters");
+		System.out.println("Error: account name must be between 3 and 30 alphanumberic characters"); //Print out saying why the account was not deleted.
 		
-		return null;
+		return null; //If account number is not valid, then account is not deleted.  Returns null.
 	}//end delete
 }//end Agent class
