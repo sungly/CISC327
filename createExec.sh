6bb4cab6bfced2ce3dafc81c03ae91ffa76b2441
TylerMizuyabu <mizuyabu@hotmail.ca>
2016-11-02 14:35:40 -0400

    got rid of trash

--- createExec.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-javac FrontEnd.java
-echo "Main-Class: FrontEnd" > MANIFEST.MF
-jar -cvmf MANIFEST.MF FrontEnd.jar *.class
-
-cat stub.sh FrontEnd.jar > simbank.exe && chmod +x simbank.exe
-
--- stub.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-java=java
-if test -n "$JAVA_HOME"; then
-    java="$JAVA_HOME/bin/java"
-fi
-exec "$java" $java_args -jar "$0" "$@"
-exit 1
