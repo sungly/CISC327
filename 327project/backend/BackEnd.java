4e83a169ae57cd5cb245490ff659d2edb1131b08
sungly <sungly.77@gmail.com>
2016-11-24 11:58:10 -0500

    backend

--- 327project/backend/BackEnd.java
+++ 327project/backend/BackEnd.java
@@ -6,7 +6,7 @@ import java.util.*;
 /*
 *The BackEnd class reads the previous day master accounts file and applies all the transactions made in the merged summary file(set
 *of daily transaction files). After all transactions have been read, BackEnd writes a new master accounts file as well as a new
-*valid account file. 
+*valid account file.
 *Date: November 14th
 *Authors: Cloud Programming
 */
@@ -14,10 +14,10 @@ public class BackEnd {
 	static ArrayList<String[]> accounts = new ArrayList<String[]>();
 	static final String MERGED_TRANSACTIONS = "./MergedTransactions.txt";
 	static final String MASTER_ACCOUNTS = "./MasterAccounts.txt";
-	
+
 	/*
-	*main is responsible for reading the original master accounts file and 
-	*storing it in an ArrayList<String[]> accounts. It then iterates through all the 
+	*main is responsible for reading the original master accounts file and
+	*storing it in an ArrayList<String[]> accounts. It then iterates through all the
 	*transactions in the merged summary file and applies all the changes to the ArrayList.
 	*Once all the transactions have been read, main writes new master accounts file and
 	*new valid account file.
@@ -34,7 +34,7 @@ public class BackEnd {
 				line = input(br);
 			}
 			br.close();
-			
+
 			//Reads each line from the merged summary file
 			br = new BufferedReader(new FileReader(MERGED_TRANSACTIONS));
 			line = input(br);
@@ -61,7 +61,7 @@ public class BackEnd {
 				}
 			}
 			br.close();
-			
+
 			//writes a new master accounts file and a new valid account file for the next session to use
 			writeFile("MasterAccounts.txt", false);
 			writeFile("../frontend/validaccounts.txt", true);
@@ -70,23 +70,25 @@ public class BackEnd {
 			e.printStackTrace();
 		}
 	}//end main
-	
+
 	/*
 	* Deposit, withdraw and transfer all share this method to perform its transaction. Checks that the account number
-	* is valid, the current amount is not negative and that the String length of the account amount is greater than 3. 
-	* 
+	* is valid, the current amount is not negative and that the String length of the account amount is greater than 3.
+	*
 	* @params accountNum	The account number from the transaction summary
 	*	  amount	The account amount from the transaction summary
 	*/
 	public static void addInt(String accountNum, int amount) {
 		int index = getAccount(accountNum);
 		if (index != -1) {
+			System.out.println("Valid Account");
 			int current = Integer.parseInt(accounts.get(index)[1]);
 			current += amount;
 			//current amount needs to be greater than 0
 			if (current < 0) {
 				System.out.println("Failed constraint: account balance is negative");
 			} else {
+				System.out.println("Withdrawn: " + amount);
 				String temp = Integer.toString(current);
 				while (temp.length() < 3) {
 					temp = "0".concat(temp);
@@ -94,8 +96,11 @@ public class BackEnd {
 				accounts.get(index)[1] = temp;
 			}
 		}
+		else{
+			System.out.println("Invalid Account");
+		}
 	}//end addInt
-	
+
 	/**
 	* createAccount adds a new account to the ArrayList<String[]> accounts if the account doesn't already exist.
 	*
@@ -112,9 +117,10 @@ public class BackEnd {
 				index++;
 			}
 			accounts.add(index, new String[] { accountNum, "000", accountName });
+			System.out.println("Account added");
 		}
 	}//end createAccount
-	
+
 	/**
 	* deleteAccount deletes an account from the ArrayList<String[]> accounts if the account exists and the account amount is exactly "000"
 	*
@@ -138,9 +144,9 @@ public class BackEnd {
 		}//end outer else
 
 	}//end deleteAccounts
-	
+
 	/*
-	* getAccount iterates through the ArrayList<String[]> accounts to check if a given account number exists or not. 
+	* getAccount iterates through the ArrayList<String[]> accounts to check if a given account number exists or not.
 	* Returns the index in the ArrayList<String[]> accounts if found, otherwise returns -1.
 	*
 	* @params accountNum	The account number from the transaction summary
@@ -153,10 +159,10 @@ public class BackEnd {
 		}
 		return -1;
 	}//end getAccount
-	
+
 	/**
 	* Takes the name of a file to write to and boolean partial to determine if its the master accounts file
-	* or the valid account file type. If partial is True, writeFile writes the validaccount.txt and only takes 
+	* or the valid account file type. If partial is True, writeFile writes the validaccount.txt and only takes
 	* the account number from the ArrayList accounts. Otherwise, it takes each value in the ArrayList accounts
 	* to write a new MasterAccounts.txt file.
 	*
@@ -175,7 +181,7 @@ public class BackEnd {
 		fw.flush();
 		fw.close();
 	}//end writeFile
-	
+
 	/**
 	*Reads a line from a given file. Returns null if the line is null. Each return String
 	*is turned into an array using split(" ").
